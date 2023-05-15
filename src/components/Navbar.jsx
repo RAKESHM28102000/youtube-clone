e731fb807e612fa94f1139e8b3d80ec2365d80dd
RAKESH M <mrakeshmrakesh885@gmail.com>
2023-05-15 19:09:11 +0530

    first commit youtube-clone

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Paper, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+      <Paper
+      sx={{
+        ml:"10px",
+        boxShadow: 'none',
+        borderTopLeftRadius:"10px",
+        borderTopRightRadius:"50%",
+        borderBottomRightRadius:"50%",
+        borderBottomLeftRadius:"10px",
+        background:"red",
+        color:"white"
+      
+      }}
+    >
+  <h4>Rakesh</h4>
+    </Paper>
+   
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
